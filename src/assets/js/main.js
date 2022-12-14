3e94f4f163f8c7252a7cdf11030eee1872f5286d
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2022-12-14 10:54:14 +0300

    finish section slider

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,28 @@
-const num = 55;
+$('.slider__wrapper').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+
+      ]
+  });
